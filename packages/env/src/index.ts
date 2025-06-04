8dd7d5454224a3250889323e14372bd5418f9028
Nguyễn Quang Thắng <tinhpt662299@gmail.com>
2025-06-04 20:05:06 +0700

    refactor(env): share client and server schema.

---
+++ packages/env/src/index.ts
@@ -0,0 +1,37 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+const shared = {
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+};
+
+const client = {
+  NEXT_PUBLIC_APP_URL: z.string().url().optional(),
+};
+
+const server = {
+  DATABASE_URL: z.string().url().optional(),
+};
+
+export const env = createEnv({
+  shared,
+  client,
+  server,
+  experimental__runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+  skipValidation: !!process.env.CI || process.env.npm_lifecycle_event === "lint",
+});
+
+export const serverEnv = {
+  NODE_ENV: env.NODE_ENV,
+  DATABASE_URL: env.DATABASE_URL,
+};
+
+export const clientEnv = {
+  NODE_ENV: env.NODE_ENV,
+  NEXT_PUBLIC_APP_URL: env.NEXT_PUBLIC_APP_URL,
+};
