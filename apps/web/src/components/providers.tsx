c888e56f42f11312823e6df9fa8d8c91b0e78f8a
Nguyễn Quang Thắng <tinhpt662299@gmail.com>
2025-06-07 10:54:22 +0700

    chore(web): remove remote config integration.

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -2,16 +2,13 @@
 
 import * as React from 'react';
 import { ThemeProvider, TooltipProvider, Toaster } from '@rp/ui';
-import { RemoteConfigProvider } from '@rp/remote-config/client';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       <TooltipProvider delayDuration={0}>
-        <RemoteConfigProvider appId="web">
-          {children}
-          <Toaster />
-        </RemoteConfigProvider>
+        {children}
+        <Toaster />
       </TooltipProvider>
     </ThemeProvider>
   );
